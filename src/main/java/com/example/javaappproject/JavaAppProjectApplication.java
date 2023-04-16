7ebec02bbd527a9faebae9a9ce84f69c4db12149
Anurag <anuragp090@gmail.com>
2023-04-16 11:25:27 +0530

    added springboot project

---
+++ src/main/java/com/example/javaappproject/JavaAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaappproject/JavaAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
